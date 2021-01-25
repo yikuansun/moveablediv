bc7c1a67335adb06a88b8f7868be763c33eb1b52
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-25 17:15:55 -0500

    wrote some code

---
+++ moveablediv-lib.js
@@ -0,0 +1,68 @@
+// moveablediv
+// by me
+class moveablediv extends HTMLElement {
+    constructor(w, h, x, y, unit_type) {
+        super();
+        this.width = w;
+        this.height = h;
+        this.x = x;
+        this.y = y;
+        this.unit_type = unit_type;
+    }
+    static get observedAttributes() { return ["width", "height", "x", "y"]; }
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch(name) {
+            case "width":
+                this.style.width = newValue.toString() + this.unit_type;
+                break;
+            case "height":
+                this.style.height = newValue.toString() + this.unit_type;
+                break;
+            case "x":
+                this.style.left = (newValue - this.width / 2).toString() + this.unit_type;
+                break;
+            case "y":
+                this.style.top = (newValue - this.height / 2).toString() + this.unit_type;
+                break;
+        }
+    }
+    connectedCallback() {
+        this.style.width = this.width.toString() + this.unit_type;
+        this.style.height = this.height.toString() + this.unit_type;
+        this.style.top = (this.y - this.height / 2).toString() + this.unit_type;
+        this.style.left = (this.x - this.width / 2).toString() + this.unit_type;
+        this.style.position = "absolute";
+    }
+    get x() {
+        return parseFloat(this.getAttribute("x"));
+    }
+    get y() {
+        return parseFloat(this.getAttribute("y"));
+    }
+    get width() {
+        return parseFloat(this.getAttribute("width"));
+    }
+    get height() {
+        return parseFloat(this.getAttribute("height"));
+    }
+    set x(newval) {
+        this.setAttribute("x", newval);
+    }
+    set y(newval) {
+        this.setAttribute("y", newval);
+    }
+    set width(newval) {
+        this.setAttribute("width", newval);
+    }
+    set height(newval) {
+        this.setAttribute("height", newval);
+    }
+};
+customElements.define( "div-moving", moveablediv );
+
+/*
+// this stuff was made for testing
+mydiv = new moveablediv(50, 50, 20, 20, "px");
+mydiv.style.backgroundColor = "red";
+document.getElementById("container").appendChild(mydiv);
+*/
